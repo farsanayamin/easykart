c8532a90f960fc5e35c03fed9955753255438f50
farsanayamin <farsana41@gmail.com>
2024-02-23 13:05:52 +0530

    update1

--- ecommerce/Ekart/Ekart/settings.py
+++ ecommerce/Ekart/Ekart/settings.py
@@ -130,3 +130,14 @@ STATICFILES_DIRS = ['Ekart/static',]
 #media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ksajeer12@gmail.com'
+EMAIL_HOST_PASSWORD = 'Kodakkatt@786!'
+EMAIL_USE_TLS = True
